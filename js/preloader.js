36238b31d2b606d0be625133fb6104e27e2e5d7d
o <94394633+edilsonpx@users.noreply.github.com>
2023-02-06 22:51:40 -0300

    Add files via upload

---
+++ js/preloader.js
@@ -0,0 +1,9 @@
+
+//<![CDATA[
+  $(window).on('load', function () {
+    $('#preloader .inner').delay(1000).fadeOut();
+    $('#preloader').delay(1000).fadeOut('slow'); 
+    $('body').delay(5).css({'overflow': 'visible'});
+  })
+  //]]>
+  
